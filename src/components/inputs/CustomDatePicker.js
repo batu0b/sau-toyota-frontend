2c385dcb1ef193648b919ed19d1dace4b6352e62
batu0b <batuhan.bahayetmez@gmail.com>
2023-04-16 03:58:25 +0300

    removed unnecessary imports

--- src/components/inputs/CustomDatePicker.js
+++ src/components/inputs/CustomDatePicker.js
@@ -1,5 +1,5 @@
 import { useField } from "formik";
-import React, { useCallback, useState } from "react";
+import React, { useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import { SelectDatepicker } from "react-select-datepicker";
 
